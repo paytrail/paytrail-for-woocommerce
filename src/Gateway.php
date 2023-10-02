fe42d4a0b7ceafea79ac18fb0708dec956b61700
juhopekkavuorela <juho-pekka.vuorela@paytrail.com>
2023-10-02 14:27:07 +0300

    Add intval ensuring we get integer

--- src/Gateway.php
+++ src/Gateway.php
@@ -1250,7 +1250,7 @@ final class Gateway extends \WC_Payment_Gateway {
 		 * @since 1.0
 		 */
 		$order_items = apply_filters('woocommerce_paytrail_gateway_get_order_items', $order->get_items([ 'line_item', 'fee', 'shipping' ]), $order);
-		$order_total = $this->helper->handle_currency($order->get_total());
+		$order_total = intval($this->helper->handle_currency($order->get_total()));
 
 		// Convert items to SDK Item objects.
 		$items = array_map(
