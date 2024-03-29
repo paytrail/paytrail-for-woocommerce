d371f268e6e1ca1ae9647e48975910fb9d1029f2
NerdCloud <devs+paytrail@nerdcloud.co>
2024-03-29 16:40:29 +0530

    Changes to add support for pay & Tokenize functionality are done

--- src/Controllers/Callback.php
+++ src/Controllers/Callback.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Paytrail for Woocommerce payment Callback controller class
  */
@@ -6,10 +7,51 @@
 namespace Paytrail\WooCommercePaymentGateway\Controllers;
 
 use Paytrail\WooCommercePaymentGateway\Gateway;
+use Paytrail\SDK\Response\GetTokenResponse;
 
 class Callback extends AbstractController {
 
 	protected function index() {
 		new Gateway(['callbackMode' => true]);
 	}
+  
+    protected function payAddCard(){
+      
+      $gateway = new Gateway();
+      
+      $gateway->log('Recieved Callback for pay and add card method', 'debug');
+      
+      	$reference = filter_input(INPUT_GET, 'checkout-reference');
+        
+        $decoded = filter_input(INPUT_GET, 'checkout-card-token');
+      
+        $gateway->log("Received Token with ID: $decoded", 'debug');
+
+      
+        // Step 2: Extract Order ID from POST data
+        if (isset($reference)) {
+          
+            $order_id = $reference;
+
+            // Step 3: Retrieve Order Details using WooCommerce API
+            $order = wc_get_order($order_id);
+          
+            // Step 4: Process Order Details
+            if ($order) {
+              
+                $user_id = $order->get_customer_id();
+              
+                $gateway->process_pay_add_card_token($decoded,$user_id);
+
+                // Example: Log the order details
+                $gateway->log("Received order with ID: $order_id, Total: $order_total, Email: $customer_email", 'debug');
+            } else {
+                // Handle the case where the order ID is not valid
+                $gateway->log("Invalid order ID received: $order_id", 'debug');
+            }
+        } else {
+            // Handle the case where the order ID is not present in the POST data
+            $gateway->log("Order ID not present in the POST data", 'debug');
+        }
+    }
 }
--- src/View/SavedPaymentMethods.php
+++ src/View/SavedPaymentMethods.php
@@ -25,14 +25,23 @@ $delete_card_url = Router::get_url(Plugin::CARD_ENDPOINT, 'delete');
 </div>
 <?php if ($has_methods) : ?>
 	<?php ( new Gateway() )->saved_payment_methods(); ?>
+
+	<p class="form-row">
+		<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+		<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" name="add_card_pay" id="add_card_pay">
+		  <span class="woocommerce-terms-and-conditions-checkbox-text"><?php esc_html_e('Save New Card and Pay', 'paytrail-for-woocommerce'); ?></span>
+		</label>
+		<input type="hidden" name="add_card_pay-field" value="1">
+	</p>
+
 	<a class="paytrail-for-woocommerce-tokenized-payment-method-links delete-card-button button"
 	   href="#"><?php esc_html_e('Delete selected card', 'paytrail-for-woocommerce'); ?></a>
 <?php endif; ?>
-<a class="paytrail-for-woocommerce-tokenized-payment-method-links add-card-button button"
+<!--<a class="paytrail-for-woocommerce-tokenized-payment-method-links add-card-button button"
    href="<?php echo esc_url($add_card_form_url); ?>">
 	<span class="paytrail-for-woocommerce-tokenized-payment-add-card-button dashicons dashicons-plus"></span>
-	<?php esc_html_e('Add new card', 'paytrail-for-woocommerce'); ?>
-</a>
+	<?php esc_html_e('Add New Card', 'paytrail-for-woocommerce'); ?>
+</a>-->
 
 <script>
 	jQuery(document).ready(function () {
