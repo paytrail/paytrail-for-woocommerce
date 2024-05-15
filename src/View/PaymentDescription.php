84f70bcf3fcd01f25dbf28cb90ea1cff04d7410f
Chris Gan <2246778+chrisgan@users.noreply.github.com>
2024-05-15 14:23:38 +0100

    Allow selection of HTML tags in the payment description view. #134

--- src/View/PaymentDescription.php
+++ src/View/PaymentDescription.php
@@ -3,12 +3,49 @@
  * Checkout payment description
  */
 
+$allowedHtml = [
+	'a'          => [
+		'href'  => true,
+		'title' => true
+	],
+	'abbr'       => [
+		'title' => true
+	],
+	'acronym'    => [
+		'title' => true
+	],
+	'b'          => [],
+	'blockquote' => [
+		'cite' => true
+	],
+	'cite'       => [],
+	'code'       => [],
+	'del'        => [
+		'datetime' => true
+	],
+	'em'         => [],
+	'i'          => [],
+	'q'          => [
+		'cite' => true
+	],
+	's'          => [],
+	'strike'     => [],
+	'strong'     => [],
+	'img'        => [
+		'src'      => true,
+		'class'    => true,
+		'alt'      => true,
+		'title'    => true,
+		'longdesc' => true
+	]
+];
+
 // Ensure that the file is being run within the WordPress context.
-if (! defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
-if (!$data['description']) {
+if ( ! $data['description'] ) {
 	return;
 }
 ?>
-<p><?php echo esc_html($data['description']); ?> </p>
+<p><?php echo wp_kses( $data['description'], $allowedHtml ); ?> </p>
